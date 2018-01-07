32fee14ee9a5f97039cf28de99bc88d5d05b0078
Mgen <m@mgenware.com>
2018-01-07 18:24:34 +0800

    Rename xxx helper methods to logXXX

--- lib/logger.ts
+++ lib/logger.ts
@@ -18,19 +18,19 @@ export default class Logger {
     this.storeBase.log(entry);
   }
 
-  error(category: string, data: any) {
+  logError(category: string, data: any) {
     this.log(category, LogLevel.Error, data);
   }
 
-  warning(category: string, data: any) {
+  logWarning(category: string, data: any) {
     this.log(category, LogLevel.Warning, data);
   }
 
-  info(category: string, data: any) {
+  logInfo(category: string, data: any) {
     this.log(category, LogLevel.Info, data);
   }
 
-  verbose(category: string, data: any) {
+  logVerbose(category: string, data: any) {
     this.log(category, LogLevel.Verbose, data);
   }
 }
